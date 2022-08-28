147267733736deb73ea00eed63640f71b9de090f
Silard Hudan <silard.gudan@gmail.com>
2022-08-28 23:03:29 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -30,19 +30,19 @@ const options = {
 flatpickr(text, options);
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
+  // millisec per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
+  // days
   const days = Math.floor(ms / day);
-  // Remaining hours
+  // hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
+  // minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
+  // seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
